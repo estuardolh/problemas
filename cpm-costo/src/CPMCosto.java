00f376794a7b3d5cc76b7ec0975f803caef9c0b1
Estuardo López <estuardolh@gmail.com>
2018-10-28 14:41:39 -0600

    nada

--- cpm-costo/src/CPMCosto.java
+++ cpm-costo/src/CPMCosto.java
@@ -50,7 +50,7 @@ public class CPMCosto {
 		if( costos == null ) throw new Exception("Los costos deben ser indicadas");
 		if( duraciones == null ) throw new Exception("Las duraciones deben ser indicadas");
 		
-		paso1();
+		//paso1();
 		paso2();
 		paso3();
 		
@@ -63,7 +63,7 @@ public class CPMCosto {
 	
 	private void paso2() {
 		int alto = tamanoDeArreglo(actividades);
-		int ancho = 2;
+		//int ancho = 2;
 		
 		this.tabla_paso2 = new double[ alto ][ 3 ];
 		
@@ -92,20 +92,27 @@ public class CPMCosto {
 	}
 	
 	private void paso3() {
+		imprimirTabla(this.actividades);
+		
 		ArrayList<String> rutas_unicas = new ArrayList<String>();
 		
-		for(int j = 1; j < tamanoDeArreglo(this.actividades); j++) {
+		for(int j = 0; j < tamanoDeArreglo(this.actividades); j++) {
 			String actividad = this.actividades[j][0];
 			String precedencia = this.actividades[j][1];
 			
-			String[] precedencias = precedencia.split(",");
-			for(int i = 0; i < precedencias.length; i++) {
-				char punto = precedencias[i].charAt(0);
-				
-				rutas_unicas.add(                   );
+			actividad = actividad==null?"":actividad;
+			if(precedencia != null) {
+				String[] precedencias = precedencia.split(",");
+				for(int i = 0; i < precedencias.length; i++) {
+					char punto = precedencias[i].charAt(0);
+					 
+					System.out.println(punto+"->"+actividad);
+				}
+			}else {
+				System.out.println(" ->"+actividad);
 			}
-			
 		}
+		
 		// obtener rutas unicas
 		for(char i = 'A'; i < 'Z'; i++) {
 			
@@ -127,5 +134,69 @@ public class CPMCosto {
 			}
 			System.out.print("]");
 		}
+		System.out.println("");
 	}
+
+	private void imprimirTabla(String[][] tabla) {
+		int alto = tamanoDeArreglo(tabla);
+		int ancho = alto==0?0:tabla[0].length;
+		
+		for(int j = 0; j < alto; j++) {
+			System.out.print("\n[");
+			for(int i = 0; i < ancho; i++) {
+				System.out.print(tabla[j][i]+" ");
+			}
+			System.out.print("]");
+		}
+		System.out.println("");
+	}
+	
+	public static void main(String[] args) {
+		String[][] actividades = new String[][]{
+			{"A", null},
+			{"B", "A"},
+			{"C", "B"},
+			{"D", "B"},
+			{"E", "C"},
+			{"F", "D"},
+			{"G", "C"},
+			{"H", "E,F"},
+			{"I", "G,H"}
+		};
+		int[][] costos = new int[][]{
+			{100,350},
+			{200,400},
+			{80,175},
+			{150,300},
+			{75,325},
+			{120,450},
+			{125,250},
+			{190,320},
+			{95,170}
+		};
+		int[][] duraciones = new int[][]{
+			{8,5},
+			{6,3},
+			{6,4},
+			{7,4},
+			{7,3},
+			{2,1},
+			{4,2},
+			{4,1},
+			{5,4}
+		};
+		
+		CPMCosto costo_cpm = new CPMCosto();
+		costo_cpm.setActividades(actividades);
+		costo_cpm.setCostos(costos);
+		costo_cpm.setDuraciones(duraciones);
+		
+		try {
+			costo_cpm.resolver();
+		} catch (Exception e) {
+			System.err.println("Error al resolver.");
+			e.printStackTrace();
+		}
+	}
+
 }
